0e69539f20b1613791f2dedb560134e10225578b
aaka8566 <aakash8566@gmail.com>
2023-04-03 03:14:20 +0530

    last

--- manyta-clone-website/src/redux/AdminReducer/Action.js
+++ manyta-clone-website/src/redux/AdminReducer/Action.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// import axios from "axios";
 import { deletedatasuccess, updatedatasuccess,adddatasuccess,datasinglesuccess,datasuccess,failure,request,loginsuccess} from "./ActionTypes";
 
 export const actionrequest=()=>{
